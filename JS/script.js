67e35b3f8445519a7e5cb20ae3a10101776bde50
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-08-27 20:20:03 -0400

    timeout

--- JS/script.js
+++ JS/script.js
@@ -5,17 +5,15 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 NOTA: non è importante l'ordine con cui l'utente inserisce i numeri, basta che ne indovini il più possibile.*/
 
 //TODO  RICUPERO DEGLI ELLEMENTI DAL DOM
-const timer = document.querySelector('h2');
+const timer = document.getElementById('time-down');
+const message = document.getElementById('message');
 const random = document.getElementById('random');
-const numbersField = document.getElementById('numbers-field');
+const input = document.querySelectorAll('input');
 const result = document.getElementById('result');
 const score = document.getElementById('score');
 
-//Todo Primo passo
-//Generare un numero random
-let totNumbers = 6;
-let max = 99;
-
+//TODO FUNCTION
+//Array con Numero Random
 const getRandomNumbers = (max, totNumbers) => {
     const numbers = [];
     while(numbers.length < totNumbers){
@@ -25,16 +23,33 @@ const getRandomNumbers = (max, totNumbers) => {
     return numbers;
   }
 
+//Todo Primo passo
+//Generare un numero random
+let totNumbers = 6;
+let max = 99;
+
 const numbers = getRandomNumbers(max , totNumbers);
 random.innerText = (numbers[0] + ' ' + numbers[1] + ' ' + numbers[2] + ' ' + numbers[3] + ' ' + numbers[4] + ' ' + numbers[5] + ' ');
 
-
 //creare un timeout
- 
-//TODO Finito il Time...
-//Nascondere i numeri random
+let seconds = 2;
+timer.innerText = seconds;
 
-//Mostrare l'input e racogliere i numeri
+const countDown = setInterval(() => {
+    timer.innerText = --seconds;
+    //TODO Finito il Time...
+    if(seconds === 0){
+        clearInterval(countDown);
+        //Nascondere i numeri random
+        random.classList.add('display-none');
+        message.innerText = 'Qualle sono i numeri?';
+        //Mostrare l'input
+        numberField.classList.remove('display-none');
+    }
+}, 1000);
+
+//Racogliere i numeri
+const memoNumber = input.value;
 
 //Paragonare i randomNumbers con i numbersField
 
